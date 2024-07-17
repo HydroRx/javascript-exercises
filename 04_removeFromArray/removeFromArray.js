f8c5f293478e891e44cbfe7eaac5993ebce0c931
Oleg D <rushalli2011@gmail.com>
2024-07-17 10:50:33 +0300

    Finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...inputs) {
+    for (const input of inputs){
+        for(let i=arr.length-1; i>=0;i--){
+            if(arr[i]===input) {
+                arr.splice(i, 1);
+            }
+        } 
+    }
+        console.log(arr)
+        return arr;
+
+   
 };
 
 // Do not edit below this line
