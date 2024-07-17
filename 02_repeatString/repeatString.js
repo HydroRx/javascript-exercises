a3356ebd53da877dd018a68739c047bbe94e2bf7
Oleg D <rushalli2011@gmail.com>
2024-07-17 10:24:03 +0300

    First exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    hola = string
+    if(num==0) {
+        return ""
+    }
+    if(num<0) {
+        return "ERROR"
+    }
+    else {
+    while(num>1) {
+        string = string + hola
+        num--;
+    }
+    return string
+}
 };
 
 // Do not edit below this line
