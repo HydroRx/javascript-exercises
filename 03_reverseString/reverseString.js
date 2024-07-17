e02ed1fee699b752dd000fb6741e4ce0d0d0f040
Oleg D <rushalli2011@gmail.com>
2024-07-17 10:35:16 +0300

    Second exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    counter = string.length
+    revstring = ''
+    while(counter>0) {
+        revstring = revstring+string.slice(-1)
+        string=string.slice(0,-1)
+        counter--;
+    }
 
+    return revstring
 };
 
 // Do not edit below this line
