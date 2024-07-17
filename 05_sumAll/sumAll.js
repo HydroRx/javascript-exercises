fc9d551c47c48565bd5a964b790a97eb8725a7aa
Oleg D <rushalli2011@gmail.com>
2024-07-17 11:39:46 +0300

    one more

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (typeof num1 != "number" || typeof num2 != "number") {
+    return "ERROR";
+  }
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } else {
+    let difference = Math.abs(num1 - num2) + 1;
+    console.log(difference);
+    let sum = 0;
+    while (difference >= 0) {
+      sum = sum + difference;
+      difference--;
+    }
 
+    return sum;
+  }
 };
 
 // Do not edit below this line
