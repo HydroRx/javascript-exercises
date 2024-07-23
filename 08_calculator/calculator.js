3879079b3a7a259fac9226328e318764780ebc20
Oleg D <rushalli2011@gmail.com>
2024-07-23 14:16:14 +0300

    2 assignments done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (...numbers) {
+  return numbers.reduce((sum, current) => sum + current, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a === 0 || a === 1) {
+    return 1;
+  } else {
+    return a * factorial(a - 1);
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const cleaned = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const reversed = cleaned.split("").reverse().join("");
+  if (cleaned === reversed) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
