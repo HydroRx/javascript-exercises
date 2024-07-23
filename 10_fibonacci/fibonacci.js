45d1cc17a63cad6520830e053857b6093e9f346b
Oleg D <rushalli2011@gmail.com>
2024-07-23 14:29:22 +0300

    anotha one

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num === 0) return 0;
+  if (num < 0) return "OOPS";
+  let fibo = [0, 1];
+  for (i = 2; i <= num; i++) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+  }
+  return fibo[num];
 };
 
 // Do not edit below this line
