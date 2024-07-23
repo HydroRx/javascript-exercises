036638342302572290e7cb6b55af9c6284ca060c
Oleg D <rushalli2011@gmail.com>
2024-07-23 14:38:44 +0300

    antha

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (titles) {
+  let titleArray = [];
+  for (i = 0; i < titles.length; i++) {
+    titleArray.push(titles[i].title);
+  }
+  return titleArray;
 };
 
 // Do not edit below this line
